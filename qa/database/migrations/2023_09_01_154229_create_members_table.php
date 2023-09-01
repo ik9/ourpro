729f095fd3651c5f688859ae52d1cf23161b469c
ik9 <hackizox@gmail.com>
2023-09-01 19:27:22 +0300

    some changes

---
+++ qa/database/migrations/2023_09_01_154229_create_members_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("email");
+            $table->string("age");
+            $table->string("as");
+            $table->string("skills");
+            $table->json("social");
+            $table->text("description");
+            $table->longText("long_description");
+            $table->longText("logo_url");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
